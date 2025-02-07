7cc7ff08b3d78f4d24c1c02742d8ff53fe2847e0
Sudhanshu9tiyal <150656173+Sudhanshu9tiyal@users.noreply.github.com>
2025-02-07 03:11:46 +0000

    datatype tk done

---
+++ basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_Id = 145768
+let accountEmail = "sudhanshu@gmail.com"
+var accountPassword  = "12345"
+accountCity = "Dehradun"
+//account_Id = 2 -----> Not allowed.
+accountEmail = "hello@gamail.com"
+accountPassword = 23456
+accountCity = "gurgaon"
+let accountState;
+
+
+console.log(account_Id);
+console.table([account_Id , accountEmail , accountPassword , accountCity , accountState])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use stricts" ; // -> treat all JS code as newer version.
+
+//  alert("hello"); -> not possible in node js we are not using browser.
+// -> code readablity should be very hight 
+// -> use mdn documentation
+ 
