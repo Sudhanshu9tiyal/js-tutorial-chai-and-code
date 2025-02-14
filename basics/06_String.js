7931f3bca82e70f1772c5659e6e2e666d95048cc
Sudhanshu9tiyal <150656173+Sudhanshu9tiyal@users.noreply.github.com>
2025-02-14 01:53:59 +0000

    strings done

---
+++ basics/06_String.js
@@ -0,0 +1,12 @@
+//const s = new String("sudhanshu")     |        {both are possible to implement all the methods of string}
+//const s = "Aman"                      |
+//console.log(s);
+//console.log(s.length);
+//console.log(s.charAt(3))
+//console.log(s.indexOf('u' , 'u'))
+
+const newstring = String("      ICHIGO KUROSAKI        ");
+console.log(newstring.trimEnd());
+
+
+
