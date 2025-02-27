bfac2d71eb9c02bfb61e80fdde4d9d113c17e621
Sudhanshu9tiyal <150656173+Sudhanshu9tiyal@users.noreply.github.com>
2025-02-27 03:17:27 +0000

    objects continuing.....

---
+++ basics/08_objects.js
@@ -0,0 +1,10 @@
+const jsuser = 
+{
+    name : "sudhanshu shekhar Nautiyal",
+    age : 56 , 
+    class : "12th",
+    loggedindays : ["monday " , "saturday"]
+    
+}
+console.log(jsuser.name);
+console.log(jsuser.class)
