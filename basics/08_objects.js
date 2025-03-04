4459247b103d12a16a1d78b3b835d8c97bdbc49c
Sudhanshu9tiyal <150656173+Sudhanshu9tiyal@users.noreply.github.com>
2025-03-04 05:03:21 +0000

    half done objects

--- basics/08_objects.js
+++ basics/08_objects.js
@@ -1,10 +1,33 @@
-const jsuser = 
-{
-    name : "sudhanshu shekhar Nautiyal",
-    age : 56 , 
-    class : "12th",
-    loggedindays : ["monday " , "saturday"]
-    
+
+
+const tinderuser = new Object(); // ---->singleton object
+// const tinderuser = {}; // ---> non singleton object.
+tinderuser.id = 78;
+tinderuser.name = "kon"
+tinderuser.isloggedin = false
+
+// console.log(tinderuser)
+
+
+const anotheruser = {
+    email : "someone@gmail.com" ,
+    course : {
+        branch: "computer application",
+        university_name : "graphic era university",
+        imp_details : {
+            student_id : 22151599,
+            student_phonenumber : 828908800
+
+
+        }
+
+    }
 }
-console.log(jsuser.name);
-console.log(jsuser.class)
+
+// console.log(anotheruser.course.imp_details.student_phonenumber)//->> object k andr objects
+obj1 = {a : "1" , b :"2"}
+obj2 = {c:"3" , d : "4"}
+obj3 = {e : "5" , f : "6"}
+
+obj4 = Object.assign({} , obj1 , obj2 , obj3)
+console.log(obj4)
