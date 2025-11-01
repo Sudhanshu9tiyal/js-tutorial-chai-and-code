e8a1ac1f1c4943e58281fad48d7d68a95dee6aba
Sudhanshu9tiyal <150656173+Sudhanshu9tiyal@users.noreply.github.com>
2025-11-01 15:56:18 +0000

    date done

---
+++ basics/date_and_time.js
@@ -0,0 +1,13 @@
+let d = new Date()
+// console.log(d.toString());
+// console.log(d.toDateString());
+// console.log(d.toISOString());
+// console.log(d.toLocaleString());
+// console.log(d.toLocaleTimeString());
+
+// custom date
+
+let mydate = new Date(2004 , 0 , 3 , 9 , 19)
+console.log(mydate.toDateString());
+
+// timestamps
